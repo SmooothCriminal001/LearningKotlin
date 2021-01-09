74e95a20bbf4ce9d1d6703d5386584d4e6f14403
hariharanu <hariharan.u@wabco-auto.com>
2021-01-09 23:02:37 +0530

    More Kotlin Class Examples

--- src/main/kotlin/ClassExamples.kts
+++ src/main/kotlin/ClassExamples.kts
@@ -4,7 +4,24 @@ import java.time.LocalDate
 import java.util.*
 
 //Class created, and three fields initialized already
-class Employee(val name: String, val age: Int, var doj: LocalDate)
+class Employee(val name: String, val age: Int, var doj: LocalDate){
+    var employmentType: String = "full time"
+    var designation: String = "Designer"
+
+    //Secondary constructor taking after primary constructor
+    constructor(name: String, age: Int, doj: LocalDate, employmentType: String) : this(name, age, doj){
+        this.employmentType = employmentType
+    }
+
+    //Secondary constructor taking after another secondary constructor
+    constructor(name: String, age: Int, doj: LocalDate, employmentType: String, designation:String) : this(name, age, doj, employmentType){
+        this.designation = designation
+    }
+
+    fun employTypeAndDesig(){
+        println("$name's employment type is $employmentType and designation is $designation")
+    }
+}
 
 //Initializing instance. No 'new' here
 //getter and setter immediately created in the background
@@ -13,9 +30,17 @@ harry.doj = LocalDate.parse("2018-12-12")
 //harry.name = "Hari"   Error, as val variable cannot be overridden
 println(harry.doj)
 println(harry.name)
+harry.employTypeAndDesig()
+
+Employee("Kumaresan", 29, LocalDate.parse("2013-03-03"), "part time").employTypeAndDesig()
+
+Employee("Dinesh", 29, LocalDate.parse("2014-04-04"), "part time", "Software Engineer").employTypeAndDesig()
 
 println("\nGetter, setters; Fields;")
-class Student(val name:String, val rollNo: String, grade: String){
+//properties are defined in constructor level
+//grade is a constructor PARAMETER. Just being devoid of 'val' or 'var' makes it a parameter
+//default property set in constructor
+class Student(val name:String, val rollNo: String, grade: String, val yearOfPassing: Int = 2020){
 
     //grade is a property created within the class
     var grade = grade
@@ -31,6 +56,19 @@ class Student(val name:String, val rollNo: String, grade: String){
     var yearEndResult: String = "Undetermined"
     private set(value){ //private set, so this property could not changed anywhere else
         field = value.toUpperCase()
+    }
+
+    //initializing a variable when it is declared. This could be used instead of 'init'
+    val rankingSystem: String = if(yearOfPassing < 2012) "Marks" else "Grades"
+
+    var presentOrPastSet: String = "Present"
+
+    //init example. This could be avoided by assigning the if block against presentOrPastSet declaration itself
+    init {
+        if(yearOfPassing < 2018){
+            presentOrPastSet = "Past"
+        }
+        presentOrPastSet = presentOrPastSet.toLowerCase()
     }
 
     fun updateYearEndResult(marks: Int){
@@ -54,8 +92,10 @@ catch (ex: Exception){
 
 println(stud1.grade)
 stud1.updateYearEndResult(20)
-println("The year end result of ${stud1.name} is ${stud1.yearEndResult}")
+println("The year end result of ${stud1.name} is ${stud1.yearEndResult}, with the grading system: ${stud1.rankingSystem}")
+println("${stud1.name} belongs to the ${stud1.presentOrPastSet} set")
 
-val stud2 = Student("Selva", "23345", "ten")
+val stud2 = Student("Selva", "23345", "ten", 2010)
 stud2.updateYearEndResult(90)
-println("The year end result of ${stud2.name} is ${stud2.yearEndResult}")
+println("\nThe year end result of ${stud2.name} is ${stud2.yearEndResult}, with the grading system: ${stud2.rankingSystem}")
+println("${stud2.name} belongs to the ${stud2.presentOrPastSet} set")
